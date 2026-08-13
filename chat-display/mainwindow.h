9b65250b249e36025dd6bb228b8f432360225a89
Yahya-Alkhosoussy <Yahya.Alkhosoussy2007@gmail.com>
2026-08-13 16:05:56 +0300

    Added current font and current colour as members of the MainWindow class, and added a struct to represent the RGBA colour values as an object.
    Added a function declaration for applySettingsChanges.

--- chat-display/mainwindow.h
+++ chat-display/mainwindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QColor>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -13,6 +14,14 @@ class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+private:
+    QFont current_font;
+    QColor current_color;
+    struct RGBA {
+        int rValue, gValue, bValue, aValue;
+        RGBA(int r, int g, int b, int a): rValue(r), gValue(g), bValue(b), aValue(a) {};
+    };
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
@@ -22,5 +31,6 @@ private:
 
     void addMessage(const QString &text); // to add a message on the display
     // helpful comment right? lol
+    void applySettingsChanges(int fontSize, QColor colour, bool transparent, RGBA backgroundColor);
 };
 #endif // MAINWINDOW_H
