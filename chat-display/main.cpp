81fd87e90b04cf4eb89f797442802956c555a7ed
Yahya-Alkhosoussy <Yahya.Alkhosoussy2007@gmail.com>
2026-08-13 04:29:54 +0300

    Added org name and application name for easier access to the settings

--- chat-display/main.cpp
+++ chat-display/main.cpp
@@ -5,6 +5,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::setOrganizationName("spiderbyte");
+    QApplication::setApplicationName("chat-display");
     MainWindow w;
     w.show();
     return QApplication::exec();
