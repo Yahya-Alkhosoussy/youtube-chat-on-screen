6014dca41e97349bfad3436135d1a997b18c7894
Yahya-Alkhosoussy <Yahya.Alkhosoussy2007@gmail.com>
2026-08-15 15:37:02 +0300

    Made a ChatWorker header file that includes the declaration of the ChatWorker class that is a child of the QObject class and has the following:
    slots:
    startPoll (public)
    stopPoll (public)
    poll (private)
    
    a constructor
    
    signals:
    messagesFetched (to tell the main thread the messages were fetched
    errorOccurred (To tell the main thread it ran into an error)

---
+++ chat-display/ChatWorker.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <QObject>
+#include <optional>
+#include <QThread>
+#include <QTimer>
+#include "ChatResponse.h"
+
+class ChatWorker : public QObject 
+{
+
+    Q_OBJECT
+
+public:
+    explicit ChatWorker(const QString &liveChatId, QObject *parent = nullptr);
+
+public slots:
+    void startPoll();
+    void stopPoll();
+
+signals:
+
+    void messagesFetched(ChatResponse response);
+    void errorOccurred(QString message);
+
+private slots:
+
+    void poll();
+
+private:
+    QString liveChatid;
+    QString nextPageToken;
+    QTimer* pollTimer = nullptr;
+};
